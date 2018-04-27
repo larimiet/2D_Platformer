fbf9a03f1abffbe5e884b5b0e1ffdc9fcd1568f8
ShootArrow <38345578+ShootArrow@users.noreply.github.com>
2018-04-27 13:42:20 +0300

    MainMenu

---
+++ 2D Platformer Devaus/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+    public List<string> changedText = new List<string>();
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D Platformer Devaus/Assets/Scripts/OnEnter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnEnter : MonoBehaviour {
+    public GameObject mainMenu;
+    public MainMenu Menu;
+    public List<string> buttonText = new List<string>();
+    public int buttonIndex;
+    public int active;
+    public Text teksti;
+	// Use this for initialization
+	void Start () {
+
+        mainMenu = GameObject.FindGameObjectWithTag("MainMenu");
+        Menu = mainMenu.GetComponent<MainMenu>();
+        buttonText = Menu.changedText;
+        active = 0;
+        teksti = gameObject.GetComponentInChildren<Text>();
+    }
+    public void activate()
+    {
+        active = 1;
+        
+    }
+    public void deactivate()
+    {
+        active = 0;
+
+    }
+    void Update () {
+        Debug.Log(buttonText[buttonIndex * 2 + active]);
+        teksti = gameObject.GetComponentInChildren<Text>();
+        teksti.text = buttonText[buttonIndex * 2 + active];
+    }
+}
