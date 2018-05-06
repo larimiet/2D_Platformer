8e729ce6c51a18d732b8db6cf751021ad9869aee
larimiet <36448899+larimiet@users.noreply.github.com>
2018-05-06 18:55:48 +0300

    Hissi liikkuu...

--- 2D Platformer Devaus/Assets/Scripts/ElevatorVertical.cs
+++ 2D Platformer Devaus/Assets/Scripts/ElevatorVertical.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class ElevatorVertical : MonoBehaviour
 {
-    // 2 = elvator goes up, 1 = elevator goes down.
+    // 1 = elvator goes up, -1 = elevator goes down.
     public int upOrDown = 1;
     public bool setUpOrDown = true;
     public LayerMask top;
     public LayerMask bottom;
+    public Vector2 targetPos;
+    public Vector2 currentPos;
+    public float speed = 10;
 
 	// Use this for initialization
 	void Start ()
@@ -31,17 +34,37 @@ public class ElevatorVertical : MonoBehaviour
 
     private void Update()
     {
+        currentPos = new Vector2(transform.position.x, transform.position.y);
+
         if(TrackEnd(transform.position, Vector2.up, 0, top))
         {
-            print("top");
+            print("im at top");
+        }
+
+        if(currentPos != targetPos)
+        {
+            Move();
+        }
+
+       RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x,
+            transform.position.y + upOrDown * 1.05f), transform.up * upOrDown);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == ("ElevatorTop") || hit.collider.gameObject.tag == ("ElevatorBottom"))
+            {
+                targetPos = (new Vector2(hit.collider.transform.position.x,
+                hit.collider.transform.position.y));
+                print("top above");
+            }
+        }
+        else
+        {
+            print("else");
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+   public void Move()
     {
-        if(CompareTag("ElevatorTop"))
-        {
-            print("ElevatorTop");
-        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, 0.2f * speed * Time.deltaTime);
     }
 }
